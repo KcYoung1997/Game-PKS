7e6ce8c60c1eae92742a81d2d21d318f94c96c33
KcYoung1997 <kcyoung1997@hotmail.co.uk>
2015-03-17 22:08:33 +0000

    sadas

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,32 @@
 package game;
 
-public class Main {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import javax.swing.JFrame;
+
+public class Main extends Canvas implements Runnable{
+	
+	public Main(){
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
